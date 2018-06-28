a28fc151eb494796cdb3b5bb424711ca3f6d0d83
Jonathan Fairfull <jonnyboris@gmail.com>
2018-06-28 18:19:54 +0100

    fix: text even clearer on mobile

--- js/ingerland.js
+++ js/ingerland.js
@@ -3,6 +3,9 @@ var $sound = document.getElementById('sound');
 var $tune = document.getElementById('tune');
 var playing = false;
 
+var time = moment("2018-07-15 18:00:00").countdown().toString();
+$e.innerText = time;
+
 setInterval(function () {
     var time = moment("2018-07-15 18:00:00").countdown().toString();
     $e.innerText = time;
