c95a02b6ebbaa455adea8e54fd012727ff176b1f
Jonny Fairfull <jonnyboris@googlemail.com>
2019-06-06 13:37:04 +0100

    Update timestamp

--- js/ingerland.js
+++ js/ingerland.js
@@ -3,11 +3,11 @@ var $sound = document.getElementById('sound');
 var $tune = document.getElementById('tune');
 var playing = false;
 
-var time = moment("2022-07-15 18:00:00").countdown().toString();
+var time = moment("2019-06-09 21:15:00").countdown().toString();
 $e.innerText = time;
 
 setInterval(function () {
-    var time = moment("2022-07-15 18:00:00").countdown().toString();
+    var time = moment("2019-06-09 21:15:00").countdown().toString();
     $e.innerText = time;
 }, 1000)
 
